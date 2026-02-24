b152f3969eb5625e545007c65f20a58bfdae7ac3
Misbah Fahamsyah <misbah_glory@yahoo.com>
2026-02-24 13:43:17 +0700

    feat: company, competition

---
+++ data.js
@@ -0,0 +1,50 @@
+const projects = [
+
+{
+    type: "company",
+    title: "Voyager",
+    overview: "Voyager is an internal initiative designed to support the XLSmart team in handling day-to-day operational and analytical tasks more efficiently. The project focuses on strengthening AI-driven workflows and operational automation to ensure reliability, scalability, and smooth integration of machine learning systems into regular business processes.",
+    role: "MLOps Engineer",
+    organization: "XLSmart Telecom Sejahtera Tbk",
+    responsibilities: "Built ML pipeline and deployed to production.",
+    skills: "MLOps, Automation, AI, Machine Learning, Cloud Infrastructure (AWS), CI/CD Pipeline, Infrastructure as Code, Solution Architecture, Software Engineering Fundamentals",
+    tools: "Python, Docker, GitLab, AWS CloudFormation, Terraform, AWS CodePipeline, AWS CodeBuild, AWS Lambda, AWS ECS, AWS EventBridge, AWS API Gateway, AWS Bedrock, AWS Transcribe, AWS Polly, AWS S3",
+    evidence: [
+        "assets/company/xquisite_analitika_indonesia/Voyager/1.png", 
+        "assets/company/xquisite_analitika_indonesia/Voyager/2.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/3.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/4.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/5.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/6.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/7.png",
+        "assets/company/xquisite_analitika_indonesia/Voyager/8.png",
+    ]
+},
+
+{
+    type: "competition",
+    title: "Big Data Hackathon BPS 2021",
+    overview: "Participated in the Big Data Hackathon organized by BPS, a national-level competition focusing on the utilization of big data and data science to address real-world public and internal BPS challenges. Achieved the Favorite Winner award for proposing an innovative and well-structured research-based solution.",
+    achievement: "Favorite Winner",
+    held_by: "Badan Pusat Statistik",
+    skills: "Data Science, Machine Learning, Research & Scientific Writing, Problem Framing, Analytical Thinking, Model Evaluation",
+    tools: "Scikit-Learn, Pandas, Python",
+    evidence: [
+        "assets/competition/big_data_hackathon_bps_2021/1.png",
+        "assets/competition/big_data_hackathon_bps_2021/2.png"
+    ]
+},
+
+{
+    type: "thesis",
+    title: "Metode DBSCAN Clustering Untuk Analisis Pola Penyebaran Petir di Pasuruan",
+    year: "2020",
+    organization: "Universitas Islam Negeri Maulana Malik Ibrahim Malang",
+    skills: "Clustering, Unsupervised Learning, Geospatial Data Analysis, Data Science",
+    tools: "Scikit-Learn, Boltek, Google Earth, ArcGIS, Python",
+    evidence: [
+        "assets/thesis/undergraduate/1.png"
+    ]
+}
+
+];
